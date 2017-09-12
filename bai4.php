a781d20ab9f411af0b1fbdd35467c3136860ec4a
zTin <quachtothinh@gmal.com>
2017-09-12 14:50:48 +0700

    truu tuong hoa du lieu

---
+++ bai4.php
@@ -0,0 +1,22 @@
+<?php
+//truu tuong hoa du lieu
+class Dog {
+
+	//Cac thuoc tinh
+	var $ten;
+	var $mau; 
+	var $giong;
+	function sua()
+	{
+
+	}
+	function vay_tai() {
+
+	}
+
+	function chay() {
+
+	}
+	
+}
+?>
